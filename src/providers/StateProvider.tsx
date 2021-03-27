68d331e7168d51997bd240aefee1de0cbe235598
陳宜庭 <studenttim@chenyitingdeMacBook-Pro.local>
2021-03-27 20:46:09 +0800

    [Context]: 1.Add Local Context Provider 2.Add Reducer

---
+++ src/providers/StateProvider.tsx
@@ -0,0 +1,12 @@
+import { createContext, useContext, useReducer } from "react";
+import { IStateValue } from "../types";
+
+const StateContext = createContext({});
+
+export const StatePorvider = ({ initialState, reducer, children }: any) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext) as [IStateValue, any];
