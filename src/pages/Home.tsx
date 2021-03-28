8154146b30efbdd17a7321519a4910d8d0cb494f
陳宜庭 <studenttim@chenyitingdeMacBook-Pro.local>
2021-03-29 00:34:06 +0800

    [Pagination]: 1.Finished Pagination Animation Effect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,10 @@ import { Header } from "../components/Header";
 import { YTEmbed } from "../components/YTEmbed";
 import { useLaunchesPastQuery } from "../generated/graphql";
 import { usePagination } from "../hooks/usePagination";
+import useRWD from "../hooks/useRWD";
 import { useStateValue } from "../providers/StateProvider";
 import "../styles/Home.scss";
+import { difference } from "../utils/difference";
 import { getYTIdFromLink } from "../utils/getYTIdFromLink";
 
 interface HomeProps {}
@@ -12,18 +14,25 @@ interface HomeProps {}
 export const Home: React.FC<HomeProps> = () => {
   const [{ limit, selected }] = useStateValue();
   const { handleOnWheel } = usePagination();
+  const device = useRWD();
   const { loading, error, data } = useLaunchesPastQuery({
     variables: { limit },
   });
   if (error) return <div>{error}</div>;
 
   const target = selected === -1 ? null : data?.launchesPast?.[selected];
-  console.log(selected);
+
   return (
-    <div className="home" onWheel={(e) => handleOnWheel(e)}>
+    <div
+      className="home"
+      onWheel={(e) => {
+        if (device !== "PC") return;
+        handleOnWheel(e);
+      }}
+    >
       <Header />
       {!target ? null : (
-        <div className="item">
+        <div id={`item-${selected}`} className="item">
           <ul className="wrapper">
             <h2 className="title">{target?.mission_name}</h2>
             <p>
@@ -37,17 +46,117 @@ export const Home: React.FC<HomeProps> = () => {
               <br />
               <span>{target.links?.article_link}</span>
             </p>
-            <YTEmbed
-              style={{
-                width: "560px",
-                height: "315px",
-                maxWidth: "100vw",
-                maxHeight: "100vh",
-                borderRadius: "10px",
-              }}
-              videoId={getYTIdFromLink(target.links?.video_link)}
-              title={target.mission_name || ""}
-            />
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            <p>
+              <span>{target?.details}</span>
+              <br />
+              <span>{target.rocket?.rocket_name}</span>
+              <br />
+              <span>{target.rocket?.rocket_type}</span>
+              <br />
+              <span>{target.launch_date_local}</span>
+              <br />
+              <span>{target.links?.article_link}</span>
+            </p>
+            {!target.links?.video_link ? null : (
+              <YTEmbed
+                className="embed__video"
+                style={{
+                  width: device === "mobile" ? "100%" : "560px",
+                  minHeight: "350px",
+                  borderRadius: "12px",
+                }}
+                videoId={getYTIdFromLink(target.links?.video_link)}
+                title={target.mission_name || ""}
+              />
+            )}
           </ul>
         </div>
       )}
