3525f0e3d3260c48b61a3e0c9e40a567c664cac7
陳宜庭 <studenttim@chenyitingdeMacBook-Pro.local>
2021-03-29 01:35:33 +0800

    [gh-pages]: Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { CustomParticle } from "./components/CustomParticle";
 import { Home } from "./pages/Home";
 import "./styles/App.scss";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <>
       <CustomParticle />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+      <Home />
+    </>
   );
 };
 
